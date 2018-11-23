887c153cf6a973672f369b75d88377bf1690c303
szymonjurczyszyn <44550901+szymonjurczyszyn@users.noreply.github.com>
2018-11-23 13:25:27 +0100

    main.c
    Programowanie cz.2 listopad 2018

--- main.c
+++ main.c
@@ -1,50 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdbool.h>
-
+#include <math.h>
 int main()
 {
-// 1. Zmienne, komunikacja z u¿ytkownikiem. Instrukcje warunkowe
-// Uwaga! Nale¿y pamiêtaæ o dodaniu odpowiednich nag³ówków do funkcji. Dla std::cin i std::cout jest to <iostream>, a dla printf i scanf jest
-// to <stdio.h> lub <cstdio.h>
-// 1.1. Zadeklarowaæ zmienne typu int, float, char. Przypisaæ do nich wartoœæ, wyœwietliæ je na ekran.
-// 1.2. Wyœwietliæ zmienn¹ typu int na ekran. Nale¿y u¿yæ metody printf i konstrukcji %d
-    int a; float b; char c;
-    a=4; b=32.32; c='d';
-    printf("Zmienna typu int: %d \n",a);
-    printf("Zmienna typu float: %f \n",b);
-    printf("Zmienna typu char: %c \n",c);
-// 1.3. Pobraæ od u¿ytkownika liczbê x. Wyœwietliæ kwadrat liczby x.
-    int e; int f;
-    printf("Podaj liczbe x, ktora program podniesie do kwadratu: ");
-    scanf("%d", &e);
-    f = pow(e,2);
-    printf("Kwadrat liczby x to %d \n",f);
-// 1.4. Zapoznaæ siê z instrukcjami warunkowymi (if, else if, else oraz switch). Pobraæ od u¿ywkownika liczbê i wyœwietliæ informacjê, czy jest
-// ona: mniejsza, wiêksza, czy równa 0.
-    int g;
-    printf("Podaj liczbe x, ktora program sprawdzi, czy jest mniejsza, wieksza lub rowna 0: ");
-    scanf("%d", &g);
-    if(g<0) printf("Podana liczba %d jest mniejsza od 0 \n",g); else if(g>0) printf("Podana liczba %d jest wieksza od 0 \n",g); else if(g==0) printf("Podana liczba %d jest rowna 0 \n",g);
-// 1.5. Pobraæ ud u¿ytkownika 3 liczby (x1, x2, x3). Wyœwietliæ informacjê, która z tych liczb jest najwiêksza.
-    int h; int i; int j;
-    printf("Porownywanie liczb \n");
-    printf("Podaj liczbe x1: ");
-    scanf("%d", &h);
-    printf("Podaj liczbe x2: ");
-    scanf("%d", &i);
-    printf("Podaj liczbe x3: ");
-    scanf("%d", &j);
-    if (h>i && h>j) printf("Liczba x1 najwieksza \n");
-    if (i>h && i>j) printf("Liczba x2 najwieksza \n");
-    if (j>i && j>h) printf("Liczba x3 najwieksza \n");
-// 1.6. Napisać prosty kalkulator. Należy pobrać od użytkownika 2 liczby oraz operator (jeden z czterech: mnożenie, dzielenie, dodawanie,
-// odejmowanie). Wymagane jest utworzenie prostego menu użytkownika. Uwaga - należy pamiętać o ułamkach (1/3 powinno dać 0.33, a nie
-// 0)
-     float a1,b1,wynik;
-    char wprowadz;
+// 2. Pêtle
+// 2.1. Zapoznaæ siê z pêtl¹ while. Pêtla ta s³u¿y do wielokrotnego wykonania fragmentu kodu przez NIEZNAN¥ Z GÓRY iloœæ razy. Pêtla
+// wykonuje siê zawsze, jeœli warunek jest prawdziwy (ewaluowany do true). Napisaæ pêtlê, w któej u¿ytkownik podaje liczbê x. Nale¿y
+// wyœwietliæ tê liczbê. Je¿eli u¿ytkownik poda 0, nale¿y zakoñczyæ program.
+bool n; int m;
+while (n==false){
+printf("Podaj liczbe x, a jezeli 0 to zakoncz program: ");
+scanf("%d",&m);
+if (m==0) n=true; else n=false;
+}
+// 2.2. Przerobić kalkulator z zadania 1.6. Teraz po każdym działaniu program powinien spytać użytkownika, czy ten chce powtórzyć
+// wszystko od początku. Jeśli tak - restartujemy kalkulator i zaczynamy od nowa. Jeśli nie - kończymy program.
 
+    float a,b,wynik;
+    char opcja, wprowadz;
+do{
     printf("\nProsty kalkulator\n");
     printf("Dodawanie znak +\n ");
     printf("Odejmowanie znak -\n ");
@@ -53,29 +28,111 @@ int main()
     printf("Wprowadz operator: ");
     scanf("%s",&wprowadz);
     printf("Wpisz liczbe x1: ");
-    scanf("%f",&a1);
+    scanf("%f",&a);
     printf("Wpisz liczbe x2: ");
-    scanf("%f",&b1);
+    scanf("%f",&b);
     switch(wprowadz){
-        case '+' : wynik=a1+b1;
+        case '+' : wynik=a+b;
                  printf("Dodawanie liczb : %f\n",wynik);
                  break;
-        case '-' : wynik=a1-b1;
+        case '-' : wynik=a-b;
                  printf("Odejmowanie is : %f\n",wynik);
                  break;
-        case '*' : wynik=a1*b1;
+        case '*' : wynik=a*b;
                  printf("Mnożenie : %f\n",wynik);
                  break;
         case '/' :
-                 wynik=a1/b1;
+                 wynik=a/b;
                  printf("Dzielenia : %f\n",wynik);
                  break;
 
         default: printf("Zle wprowadzone dane\n");
 
     }
+    printf("Chcesz powtorzyc ? (t/n)\n");
+    opcja=getche();
 
+    }while(opcja=='t');
+// 2.3. Pobrać od użytkownika liczbę x. Należy wyświetlić wszystkie kolejne potęgi liczby x. Zakończyć program, kiedy liczba przekroczy 100
+// 000.
+printf("Podaj liczbe x, aby wyswietlic wszystkie kolejne potegi: ");
+int c; int a2; int b2;
+scanf("%d",&a2);
+for (b2=1;;b2++) {
+      c = pow(a2,b2);
+      printf("Liczba %d do potegi ",a2);
+      printf("%d", b2);
+      printf(" = %d \n",c);
+if(c>100000 || a2==0 || a2==1) break;
+}
+// 2.4. Pobrać od użytkownika liczbę x. Jeśli liczba jest niedodatnia - należy zakończyć program. Następnie należy wyświetlać kolejno (w
+// nowej linii) wszystkie liczby mniejsze od x, aż osiągnięte zostanie 0.
+int x1;
+printf("Podaj liczbe x, beda wyswietlane wszystkie liczby mniejsze od x do 0: ");
+scanf("%d",&x1);
+if (x1<0) {
+printf("Podana zostala liczba ujemna wiec jest zakonczenie programu \n");
+return 0;
+}
+x1=x1-1;
+for (;x1>=0;x1--) {
+printf("Wszystkie liczby mniejsze od podanej x to kolejno: %d \n", x1);
+}
+// 2.5. Zapoznać się z instrukcjami continue; i break;. Pierwsza służy do zakończenia iteracji i przejścia do następnej, a druga natychmiast
+// kończy pętlę. Przerobić program z zadania 1.4, aby wyświetlał tylko parzyste liczby (użyć if oraz continue). Jeśli liczba wynosi 40, należy
+// wyjść z pętli i zakończyć program.
+ int g;
+int g1;
+for (g1=0;;g1++){
+printf("Podaj liczbe x parzysta, ktora program sprawdzi, czy jest mniejsza, wieksza lub rowna 0: ");
+scanf("%d", &g);
+if (g % 2 ==0) {
+if(g<0) printf("Podana liczba %d jest mniejsza od 0 \n",g); else if(g>0) printf("Podana liczba %d jest wieksza od 0 \n",g); else if(g==0) printf("Podana liczba %d jest rowna 0 \n",g);
+continue;
+} else break;
+}
+// 2.6. Zapoznać się z pętlą for. Pętla służy do wykonania danego fragmentu kodu ZNANĄ Z GÓRY określoną ilość razy. Uwaga - każdą pętlę
+// for można zastąpić pętlą while i wzajemnie. Zwyczajowo obowiazuje zasada, że jeśli ilość iteracji jest znana - używamy for, a jeśli nie jest
+// znana (zależy od użytkownika lub jakiegoś stanu zewnętrznego) - używamy pętli while. Pętla for składa się z: ZMIENNEJ ITERACYJNEJ,
+// czyli stanu początkowego pętli, np. int i = 0. Następnie WARUNKU ZAKOŃCZENIA PĘTLI, np. i<10. Następnie OPERACJI WYKONYWANEJ
+// PO KAŻDEJ ITERACJI - zazwyczaj jest to zwiększenie iteratora o 1, czyli i = i + 1, bądź w skrócie i++. Napisać pętlę for, która wyświetli cyfry
+// od 0 do 9.
+int g2;
+for (g2=0;g2<=9;g2++) printf("%d \n", g2);
+// 2.7. Pobrać od użytkownika liczby x i y. Zakładamy, że y > x (zawsze, nie musimy tego sprawdzać). Za pomocą pętli for wypisać liczby
+// między x a y (do przemyślenia - co jest stanem początkowym, a co warunkiem zakończenia pętli?)
+do{
+printf("Liczby miedzy x a y: \n");
+printf("Podaj x: ");
+int x2; int y2;
+scanf("%d",&x2);
+printf("Podaj y: ");
+scanf("%d",&y2);
+printf("x= %d y= %d\n",x2,y2);
+if (y2>x2) {
+x2++;
+for (;y2>x2;x2++) printf("Liczby pomiedzy y a x to: %d\n", x2);
+} else {
+y2++;
+for (;y2<x2;y2++) printf("Liczby pomiedzy x a y to: %d\n", y2);
+}
+printf("Chcesz powtorzyc ? (t/n)\n");
+    opcja=getche();
 
-
-    return 0;
+    }while(opcja=='t');
+// 2.8. Pobrać od użytkownika liczbę x. Za pomocą pętli for wypisać wszystkie liczby mniejsze od x, ale większe niż 0.
+int x3;
+printf("Podaj x, aby program wyswietlil mniejsze liczby ale wieksze od 0: ");
+scanf("%d",&x3);
+x3--;
+for (;x3>0;x3--) printf("Wszystkie liczby mniesze od x, ale wieksze niz 0: %d\n", x3);
+// 2.9. Pobrać od użytkownika liczbę x. Za pomocą pętli for wypisywać CO TRZECIĄ liczbę większą od x. Pętla powinna się zakończyć, jeśli
+// liczba przekroczy 100. Uwaga - proszę nie używać operacji continue i break, a sterować tylko parametrami pętli.
+int x4;
+printf("Podaj x, aby program wyswietlil co 3 liczbe wieksza od x do przekroczenia 100: ");
+scanf("%d",&x4);
+x4=x4+3;
+for(;x4<100;x4=x4+3) printf("Co 3 liczba wieksza od x ale do przekroczenia 100 %d\n", x4);
+system("pause");
+return 0;
 }
